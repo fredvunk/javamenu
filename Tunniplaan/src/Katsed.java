434bf0313bdced6b4c49a5e47a1f8d6b314b2b40
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-11-25 18:06:43 +0200

    Update Katsed.java

--- Tunniplaan/src/Katsed.java
+++ Tunniplaan/src/Katsed.java
@@ -1,70 +1,126 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class Katsed {
     public static void main(String[] args) {
-        // Loo Tund objekt
-        Tund annaTund = new Tund();
-        // Lisa väärtused
-
+        // 1. tund
+        // loo Tund objekt
         Tund esimeneTund = new Tund();
-        esimeneTund.tund = "1";
-        esimeneTund.algus = "11:5";
-        esimeneTund.lopp = "14:00";
-        esimeneTund.aine = "JAVA programmeerimine";
+        // lisa väärtused
+        esimeneTund.tund = "2";
+        esimeneTund.algus = "08:30";
+        esimeneTund.lopp = "10:00";
+        esimeneTund.aine = "Mobiilirakenduse kasutajaliidese disain";
         esimeneTund.grupp = "VS18";
         esimeneTund.opetaja = "Ly Otsa";
-        esimeneTund.ruum = "A418";
+        esimeneTund.ruum = "Kopli A - A418 (arvutiklass)";
 
+        // 2. tund
+        // loo Tund objekt
         Tund teineTund = new Tund();
-        teineTund.tund = "2";
-        teineTund.algus = "11:5";
-        teineTund.lopp = "14:00";
-        teineTund.aine = "JAVA programmeerimine";
+        // lisa väärtused
+        teineTund.tund = "3";
+        teineTund.algus = "10:15";
+        teineTund.lopp = "11:45";
+        teineTund.aine = "Mobiilirakenduse kasutajaliidese disain";
         teineTund.grupp = "VS18";
         teineTund.opetaja = "Ly Otsa";
-        teineTund.ruum = "A418";
+        teineTund.ruum = "Kopli A - A418 (arvutiklass)";
 
+        // 3. tund
+        // loo Tund objekt
         Tund kolmasTund = new Tund();
-        kolmasTund.tund = "3";
-        kolmasTund.algus = "11:5";
+        // lisa väärtused
+        kolmasTund.tund = "4";
+        kolmasTund.algus = "11:55";
         kolmasTund.lopp = "14:00";
         kolmasTund.aine = "JAVA programmeerimine";
         kolmasTund.grupp = "VS18";
         kolmasTund.opetaja = "Anna Karutina";
-        kolmasTund.ruum = "A411";
+        kolmasTund.ruum = "Kopli A - A411 (arvutiklass)";
 
-        annaTund.tund = "4";
-        annaTund.algus = "11:5";
-        annaTund.lopp = "14:00";
-        annaTund.aine = "JAVA programmeerimine";
-        annaTund.grupp = "VS18";
-        annaTund.opetaja = "Anna Karutina";
-        annaTund.ruum = "A411";
+        // 4. tund
+        // loo Tund objekt
+        Tund neljasTund = new Tund();
+        // lisa väärtused
+        neljasTund.tund = "5";
+        neljasTund.algus = "14:10";
+        neljasTund.lopp = "15:40";
+        neljasTund.aine = "JAVA programmeerimine";
+        neljasTund.grupp = "VS18";
+        neljasTund.opetaja = "Anna Karutina";
+        neljasTund.ruum = "Kopli A - A411 (arvutiklass)";
 
-        Tund neljasTund =  new Tund();
-        neljasTund.tund = "4";
-        neljasTund.algus = "4";
-        neljasTund.lopp = "4";
-        neljasTund.aine = "4";
-        neljasTund.grupp = "4";
-        neljasTund.opetaja = "4";
-        neljasTund.ruum = "4";
-
+        // 5. tund
+        // loo Tund objekt
         Tund viiesTund = new Tund();
-        viiesTund.tund = "5";
-        viiesTund.algus = "11:5";
-        viiesTund.lopp = "14:00";
+        // lisa väärtused
+        viiesTund.tund = "6";
+        viiesTund.algus = "15:45";
+        viiesTund.lopp = "17:15";
         viiesTund.aine = "JAVA programmeerimine";
         viiesTund.grupp = "VS18";
         viiesTund.opetaja = "Anna Karutina";
-        viiesTund.ruum = "A411";
+        viiesTund.ruum = "Kopli A - A411 (arvutiklass)";
 
-        Paev esmaspaev = new Paev();
-        esmaspaev.kuupaev = "2019-11-18";
+        // koostame nimekiri tundidest, mis võivad toimuda päeval
         ArrayList<Tund> tunnid = new ArrayList<Tund>();
         tunnid.add(esimeneTund);
         tunnid.add(teineTund);
         tunnid.add(kolmasTund);
-        esmaspaev.tundideNimekiri = tunnid;
+        tunnid.add(neljasTund);
+        tunnid.add(viiesTund);
+
+        // loome tunniplaan tundide nimekirja abil
+        Tunniplaan vs18 = new Tunniplaan();
+//        vs18.nadal = "2019-11-18";
+//        vs18.tunnid = new HashMap<String, List<Tund>>();
+//        vs18.tunnid.put("2019-11-18", tunnid);
+
+        // kontrollime tunniplaani andmed
+//        System.out.println(vs18);
+
+        // loome JSON andmestik loodud tüübide põhjal
+        Gson g = new Gson();
+        String vs18JSON = g.toJson(vs18);
+//        System.out.println(vs18JSON);
+
+        // tunniplaani json lugemine
+        try {
+            URL url = new URL("https://tkhk.siseveeb.ee/veebilehe_andmed/tunniplaan/tunniplaan?nadal=18.11.2019&grupp=1282");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String str = "";
+            String result = "";
+            while (null != (str = br.readLine())) {
+                result += str;
+            }
+            // siin kohal olemas vajalikud andmed
+            // kasutame need tunniplaani ehitamiseks
+            Gson tunniplaaniJSON = new Gson();
+            // loeme andmed meie loodud struktuuri sisse
+            vs18 = tunniplaaniJSON.fromJson(result, Tunniplaan.class);
+            for (Map.Entry<String, List<Tund>> element: vs18.tunnid.entrySet()){
+                // näitame kuupäevad
+                System.out.println(element.getKey());
+                // näitame antud kuupäeva tunnid
+                for (Tund tund: element.getValue()) {
+                    System.out.println(tund);
+                    System.out.println("------------------");
+                }
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
 
     }
 }
